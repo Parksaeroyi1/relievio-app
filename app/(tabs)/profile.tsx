16bb800b5ca2feac9c6d928332d275017b019ade
Parksaeroyi1 <142122264+Parksaeroyi1@users.noreply.github.com>
2025-05-09 00:34:16 -0400

    update1

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function ProfileScreen() {
+  
+  return (
+    <ScrollView style={ styles.container }>
+      <SafeAreaView>
+      {/* Header */}
+      <View>
+      <Text style={ styles.title }>Profile</Text>
+      </View>
+      </SafeAreaView>
+
+
+      {/* Progress Card */}
+      <View>
+        <Text style={ styles.subTitle }> Your Progress </Text>
+        <View style={ styles.progressCard}>
+          <Text> 0% </Text>
+        </View>
+      </View>
+
+      {/* Mood Board */}
+      <View>
+        <Text style={ styles.subTitle }> Mood Board </Text>
+        <View style={ styles.progressCard}>
+          <Text> Happ, Ok, Sad </Text>
+        </View>
+      </View>
+
+
+    </ScrollView>
+)}  
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  subTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  progressCard: {
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+
+})
