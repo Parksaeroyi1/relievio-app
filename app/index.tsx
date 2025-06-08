a21684fe5d92c33ea11664bdcf2afd36be6c1af7
Elijah <142122264+Parksaeroyi1@users.noreply.github.com>
2025-06-08 02:04:57 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-// app/index.js
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 
@@ -6,7 +5,6 @@ export default function Index() {
   const router = useRouter();
 
   useEffect(() => {
-    // Delay to ensure RootLayout mounted
     const timer = setTimeout(() => {
       router.replace('/(auth)/login');
     }, 0);
